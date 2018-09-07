560be2a96be948906925e2e02eb36e5bd07d309d
Joshua E. Roybal <30689915+joshroybal@users.noreply.github.com>
2018-09-07 16:36:50 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -v
-cp 7500.dat tree.dat
-time java TestBST
-cp 7500.dat tree.dat
-time java TestAVL
+time java TestBST colors.txt
+time java TestAVL colors.txt
+time java TestBST tiny.txt
+time java TestAVL tiny.txt
+time java TestBST small.txt
+time java TestAVL small.txt
+time java TestBST medium.txt
+time java TestAVL medium.txt
+time java TestBST large.txt
+time java TestAVL large.txt
